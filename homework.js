54d5eca2210bfb0a7debe826107ae86628c17cd1
remedeae <sanna.m.blomdahl@gamail.com>
2025-08-25 16:38:13 +0200

    finished extra training + advanced assignment

--- homework.js
+++ homework.js
@@ -1,5 +1,22 @@
-let firstName = "Maya";
-let favoriteNumber = 42;
-let likesCoding = true;
+const firstName = "Maya";
+const favoriteNumber = 42;
+const likesCoding = true;
 
 console.log(`Hi, my name is ${firstName}, my favorite number is ${favoriteNumber}, and it is ${likesCoding} that I like coding.`); 
+
+const birthYear = 1991;
+const currentYear = 2025;
+
+console.log("I have been alive for", currentYear - birthYear, "years, somehow.");
+
+const foods = ["sushi", "BANANA!", "gröt"]
+const lastName = "Blomdahl"
+const student = {
+    name: firstName + " " + lastName,
+    favoriteNumber: favoriteNumber,
+    likesCoding: likesCoding,
+    niceFood: foods
+};
+
+console.log(student);
+console.log(firstName, "thinks that it's very fun to yell", student.niceFood[1], "at passing stangers.")
