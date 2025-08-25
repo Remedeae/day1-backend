e13cc3c7427e4431562dd59d15d4aed786b828c7
remedeae <sanna.m.blomdahl@gamail.com>
2025-08-25 16:05:24 +0200

    frist commit

---
+++ homework.js
@@ -0,0 +1,5 @@
+let firstName = "Maya";
+let favoriteNumber = 42;
+let likesCoding = true;
+
+console.log(`Hi, my name is ${firstName}, my favorite number is ${favoriteNumber}, and it is ${likesCoding} that I like coding.`); 
